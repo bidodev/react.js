0d003096792cba8b9a254fc2fff23ae016f02214
Claudinei Bido <bido.c@live.com>
2020-06-14 12:29:33 +0200

    search-field

--- monster-rolodex/src/App.js
+++ monster-rolodex/src/App.js
@@ -6,12 +6,15 @@ import { baseURL } from './config';
 import CardList from './components/card-list/card-list-component';
 import './components/card-list/card-list.styles.scss';
 
+import Search from './components/search-field/search-field.component';
+
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
       monsters: [],
+      searchField: '',
     };
   }
 
@@ -28,8 +31,16 @@ class App extends Component {
     const { monsters } = this.state;
 
     return (
-      <div className="card-list">
-        <CardList monsters={monsters} />
+      <div className="App">
+        <input
+          type="search"
+          placeholder="Filter cats by Name.."
+          onChange={(event) => this.setState({searchField: event.target.value}, ()=> console.log(this.state.searchField))}
+        />
+        <div className="card-list">
+          {/* <Search /> */}
+          <CardList monsters={monsters} />
+        </div>
       </div>
     );
   }
