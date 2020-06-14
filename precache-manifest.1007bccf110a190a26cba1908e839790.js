7f17bd5b53aa2d94f6f23c053ab35f4b2a73becf
Claudinei Bido <bido.c@live.com>
2020-06-14 14:00:30 +0200

    Updates

---
+++ precache-manifest.1007bccf110a190a26cba1908e839790.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ab629c5d0e5c9ef718ef7cb7ee6f0b8",
+    "url": "/cats-rolodex/index.html"
+  },
+  {
+    "revision": "e018ec8bab56a0ab8b25",
+    "url": "/cats-rolodex/static/css/main.da219fb9.chunk.css"
+  },
+  {
+    "revision": "d3290f74e1ebeacaf728",
+    "url": "/cats-rolodex/static/js/2.ed1c49cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cats-rolodex/static/js/2.ed1c49cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e018ec8bab56a0ab8b25",
+    "url": "/cats-rolodex/static/js/main.bfcdf8ce.chunk.js"
+  },
+  {
+    "revision": "ae0af4f57c299953bdfd",
+    "url": "/cats-rolodex/static/js/runtime-main.f6618131.js"
+  }
+]);
